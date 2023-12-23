d0c32fdd75c19c99e2be3c4dacfa9dcb80ca41f0
Enes BAŞ <eenesbass60@gmail.com>
2023-12-23 20:59:43 +0300

    Add project files.

---
+++ TechCareerBootcampWebApi2/Controllers/ProductController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace TechCareerBootcampWebApi2.Controllers
+{
+	[Route("api/[controller]")]
+	[ApiController]
+	public class ProductController : ControllerBase
+	{
+		[HttpGet]
+		public IActionResult GetAll()
+		{
+			return Ok("Get all products");
+		}
+
+		[HttpGet("{id}")]
+		public IActionResult GetById(int id)
+		{
+			return Ok($"Get product with id {id}");
+		}
+
+		[HttpPost]
+		public IActionResult Create()
+		{
+			return Ok("Add a new product");
+		}
+
+		[HttpDelete("{id}")]
+		public IActionResult Delete(int id)
+		{
+			return Ok($"Delete product with id {id}");
+		}	
+	}
+}
